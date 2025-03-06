eec4821ef7428e2682a47280b6859d2e02df897f
Thuy Linh <23010633@st.phenikaa-uni.edu.vn>
2025-03-07 00:29:25 +0700

    init

--- StudentManagement/resources/views/partials/main.blade.php
+++ StudentManagement/resources/views/partials/main.blade.php
@@ -3,10 +3,7 @@
     <h1 class="h3 mb-0 text-gray-800">Admin - Dashboard</h1>
 </div>
 
-<!-- Welcome Message -->
-<div class="alert alert-primary" role="alert">
-    🎉 Chào mừng Admin! Chúc bạn một ngày làm việc hiệu quả! 🚀
-</div>
-
 <!-- Content Row -->
 <div class="row">
+
+</div>
--- StudentManagement/resources/views/students/info.blade.php
+++ StudentManagement/resources/views/students/info.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
 
 <head>
+
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -10,13 +11,12 @@
 
     <title>Admin - Dashboard</title>
 
-    <!-- Custom fonts for this template-->
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
-    <!-- Custom styles for this template-->
     <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('vendor/datatables/dataTables.bootstrap4.min.css')}}" rel="stylesheet">
+    <link href="{{ asset('vendor/datatables/dataTables.bootstrap4.min.css') }}" rel="stylesheet">
+
 </head>
 
 <body id="page-top">
@@ -56,7 +56,15 @@
                                     </div>
                                     <div class="card-body">
                                         <ul class="list-group list-group-flush">
-                                            <li class="list-group-item"><strong>Mã môn học: </strong>{{ $student->subjects->first()->IdSubject ?? 'Chưa có môn học' }}</li>
+                                            <li class="list-group-item">
+                                                <strong>Mã môn học: </strong>
+                                                @if($student->subjects->isNotEmpty())
+                                                    @foreach($student->subjects as $subject)
+                                                        <span class="badge badge-primary">{{ $subject->IdSubject }}</span>
+                                                    @endforeach
+                                                @else
+                                                    <span class="text-danger">Chưa có môn học</span>
+                                                @endif
                                             <li class="list-group-item">
                                                 <strong>Môn học: </strong>
                                                 @if($student->subjects->isNotEmpty())
